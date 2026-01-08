387cf564716f967755af47c62eb96b622b7432f6
ckuthyar <ckuthyar@gmail.com>
2026-01-09 00:38:28 +0530

    Mathematical tables in Kotlin

---
+++ Test1/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    for(i in 1..10) {
+        println(3.toString()+"*" + i + "="+(3*i));
+    }
+}
