e20c8b573141c0a6b95b6eaf446cf6a43b61c7d1
ckuthyar <ckuthyar@gmail.com>
2026-01-09 01:16:05 +0530

    declaration first assignment later

--- Test1/src/K3.kt
+++ Test1/src/K3.kt
@@ -1,5 +1,11 @@
 fun main(){
     val name="Mary";
     val age=20;
-    print("$name is $age years old");
+    print("$name is $age years old\n");
+
+    val name2: String;
+    val age2: Int;
+    name2="Mary";
+    age2=20;
+    print("$name2 is $age2 years old\n");
 }
