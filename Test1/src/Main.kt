4768d0e78c2fe214ac3f8db5a6784b63e1a6b312
ckuthyar <ckuthyar@gmail.com>
2026-01-09 00:58:52 +0530

    Nested FOR loop

--- Test1/src/Main.kt
+++ Test1/src/Main.kt
@@ -1,7 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    for(i in 1..10) {
-        println(3.toString()+"*" + i + "="+(3*i));
+    val num1=3;
+    val num2=20;
+    for(j in num1..num2){
+        for(i in 1..10) {
+            println(j.toString()+"*"+i+"="+(j*i));
+        }
+        println();
     }
+
 }
