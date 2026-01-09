1e0a40c0ae0c3c3a5e408944c7c6161db4a6278d
ckuthyar <ckuthyar@gmail.com>
2026-01-09 11:22:19 +0530

    while loop do while loop. Bake a cake while cakesBaked < 5

---
+++ Test1/src/K9.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var cakesEaten=0
+    while(cakesEaten<3){
+        println("Eat a cake")
+        cakesEaten++
+    }
+    println("Next program")
+
+    var cakesBaked=0
+    do{
+        println("Bake a cake")
+        cakesBaked++
+    }while(cakesBaked<5)
+}
