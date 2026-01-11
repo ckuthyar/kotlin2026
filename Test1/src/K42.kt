6cae620674499527b542b27b859379c6446163f1
ckuthyar <ckuthyar@gmail.com>
2026-01-11 18:52:50 +0530

    comparing height of Alice and Bob

---
+++ Test1/src/K42.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val heightAlice=160
+    val heightBob=175
+
+    val taller=if(heightAlice>heightBob){
+        print("Choose Alice\n")
+        heightAlice
+    }else{
+        print("Choose Bob\n")
+        heightBob
+    }
+    println("Taller height is $taller")
+}
