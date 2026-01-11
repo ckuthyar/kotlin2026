5229c44e9087db3d10c43664443c54703b450e1f
ckuthyar <ckuthyar@gmail.com>
2026-01-11 15:12:53 +0530

    array compare using .contentEquals()

---
+++ Test1/src/K26.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val arr1=arrayOf(2,4,6,8)
+    val arr2=arrayOf(2,4,6,8)
+    var arr3=arrayOf(2,4,6)
+    println(arr1.contentEquals(arr2))
+    println(arr2.contentEquals(arr3))
+}
