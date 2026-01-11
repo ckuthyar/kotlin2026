d18a5431ccf6ef539d8f281aedbf4690d3034c3a
ckuthyar <ckuthyar@gmail.com>
2026-01-11 15:24:59 +0530

    array functions sum,average,min,max, array attribute size, array get(index)

---
+++ Test1/src/K27.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val arr1=arrayOf(2,4,6,8)
+    println(arr1.sum())
+    println(arr1.average())
+    println(arr1.min())
+    println(arr1.max())
+    println(arr1.size)
+    println(arr1.get(0))
+}
