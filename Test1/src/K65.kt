09de545fda1cd64e2af29b778755482b61133081
ckuthyar <ckuthyar@gmail.com>
2026-01-12 09:45:17 +0530

    K65 throws an exception, K66 does not throw exception since we are using .toIntOrNull() function

---
+++ Test1/src/K65.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val s1="This is not a number"
+    val num1=s1.toInt()
+    print("Num1 is $num1")
+}
+//You get NumberFormatException
---
+++ Test1/src/K66.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val s1="This is not a number"
+    val num1=s1.toIntOrNull()
+    print("Num1 is $num1")
+}
+//Output is "Num1 is null"
