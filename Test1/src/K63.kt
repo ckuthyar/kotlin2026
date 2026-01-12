e03363fb241ab3a85a42727b67a1f41953d51be0
ckuthyar <ckuthyar@gmail.com>
2026-01-12 09:29:32 +0530

    avoiding an exception using the firstOrNull() function

---
+++ Test1/src/K63.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val list1=listOf<Int>()
+    val item1=list1.first()
+    print("First item is $item1")
+
+    //this results in an exception
+}
---
+++ Test1/src/K64.kt
@@ -0,0 +1,6 @@
+fun main(){
+    val list1=listOf<Int>()
+    val item1=list1.firstOrNull()
+    println("First item is $item1")
+}
+//output is "First item is null"
